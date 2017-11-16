3cfd784ca02dcdb2da2861a6d4360ec33557db9e
david <davvid.davids@gmail.com>
2017-11-16 20:16:42 +0200

    data filter on node

--- nod.py
+++ nod.py
@@ -2,6 +2,7 @@ import asyncio
 import json
 import multiprocessing
 import logging
+import node_data
 
 LOGGER = logging.getLogger(__name__)
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
@@ -10,30 +11,42 @@ data = {
 }
 
 class Node:
-    def __init__(self, port, mast, slaves, ip='localhost',):
+    def __init__(self, port, mast, slaves, data, ip='localhost'):
         self.ip = ip
         self.port = port
         self.loop = asyncio.get_event_loop()
         self.master = mast
         self.slaves = slaves
+        self.data = data
 
     @asyncio.coroutine
     def responde(self, reader, writer):
         message = json.loads((yield from reader.read(1024)).decode('utf-8'))
-        filter = message.get('filter')
-        data = []
+        filt = message.get('filter')
         if self.master:
+            nr = len(self.slaves)
+            n=1
+            data = []
+            if filt:
+                data += self.filter(self, filt)
+            else:
+                data = self.data
             for slave in self.slaves:
+                n = n+1
                 payload = json.dumps({
                     'type': 'command',
                     'command': 'get',
-                    'filter': filter
+                    'filter': filt
                 }).encode('utf-8')
+                LOGGER.info('Creating connection...')
                 node_r, node_w = yield from asyncio.open_connection('localhost', slave, loop=self.loop)
+                LOGGER.info('Sennding payload...')
                 node_w.write(payload)
                 node_resp = yield from node_r.read(1024)
-                LOGGER.info('Recived response: ', node_resp)
-                data += json.loads(node_resp.decode().get('payload'))
+                data += json.loads(node_resp.decode()).get('payload')
+                print("Recived: ", data)
+                if n == nr:
+                    break
             payload = json.dumps({
                 'type': 'response',
                 'payload': data,
@@ -41,7 +54,7 @@ class Node:
             writer.write(payload)
             yield from writer.drain()
         else:
-            data = []
+            data = self.filter(self=self, filt=filt)
             payload = json.dumps({
                 'type': 'response',
                 'payload' : data,
@@ -65,30 +78,30 @@ class Node:
         self.loop.close()
 
     @staticmethod
-    def filter(self, data, filt):
+    def filter(self, filt):
         field = filt['field']
         op = filt['op']
         val =filt['val']
         filtered = []
-        for dat in data:
+        for dat in self.data:
             operate = getattr(dat[field], op)
             if operate(val):
                 filtered.append(dat)
         return filtered
 
 
-def start_node(master, port, slaves):
-    node =Node(mast=master, port=port, slaves=slaves)
+def start_node(master, port, slaves, data):
+    node =Node(mast=master, port=port, slaves=slaves, data=data)
     node.start()
 
 
 if __name__ == "__main__":
-    node1 = multiprocessing.Process(target=start_node, args=(False, 1111, []))
-    node2 = multiprocessing.Process(target=start_node, args=(True, 1112, [1111, 1113, 1114]))
-    node3 = multiprocessing.Process(target=start_node, args=(False, 1113, []))
-    node4 = multiprocessing.Process(target=start_node, args=(True, 1114, [1112, 1115, 1116]))
-    node5 = multiprocessing.Process(target=start_node, args=(False, 1115, []))
-    node6 = multiprocessing.Process(target=start_node, args=(False, 1116, []))
+    node1 = multiprocessing.Process(target=start_node, args=(False, 1111, [], node_data.node1_data))
+    node2 = multiprocessing.Process(target=start_node, args=(True, 1112, [1111, 1113, 1114], node_data.node2_data))
+    node3 = multiprocessing.Process(target=start_node, args=(False, 1113, [], node_data.node3_data))
+    node4 = multiprocessing.Process(target=start_node, args=(True, 1114, [1112, 1115, 1116], node_data.node4_data))
+    node5 = multiprocessing.Process(target=start_node, args=(False, 1115, [], node_data.node5_data))
+    node6 = multiprocessing.Process(target=start_node, args=(False, 1116, [], node_data.node6_data))
 
     node1.start()
     node2.start()
