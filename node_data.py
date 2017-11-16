21cad1f09be5dbfce57f668f3cd21404bead40c2
david <davvid.davids@gmail.com>
2017-11-16 20:17:01 +0200

    data for populating the nodes

---
+++ node_data.py
@@ -0,0 +1,35 @@
+node1_data = [{"name": "Daniel Vasile", "age": 14},
+              {"name": "Ion Ionescu", "age": 11},
+              {"name": "Jora Cardan", "age": 44},
+              {"name": "Aliona Berbec", "age": 19},
+              {"name": "Colea Tastiera", "age": 70}]
+
+node2_data = [{"name": "Gheorghe Onar", "age": 4},
+              {"name": "Ion Petrescu", "age": 11},
+              {"name": "Jessica Vasea", "age": 32},
+              {"name": "Alex Bezu", "age": 19},
+              {"name": "Mihail Negru", "age": 78}]
+
+node3_data = [{"name": "Tudor Jardi", "age": 22},
+              {"name": "Alexandru Q", "age": 31},
+              {"name": "Dorin Baran", "age": 40},
+              {"name": "Alina Ion", "age": 9}]
+
+node4_data = [{"name": "Dan U", "age": 50},
+              {"name": "Ian Oasea", "age": 61},
+              {"name": "Jose Huan", "age": 15},
+              {"name": "Aladin Han", "age": 90},
+              {"name": "Tudose Der", "age": 10}]
+
+node5_data = [{"name": "Enasio Bun", "age": 22},
+              {"name": "Ingrid Popescu", "age": 81},
+              {"name": "Verone Universal", "age": 4},
+              {"name": "Huaquim Benasio", "age": 17},
+              {"name": "Ursula Gavn", "age": 30}]
+
+node6_data = [{"name": "Pinder Cusoc", "age": 54},
+              {"name": "Slavic J", "age": 21},
+              {"name": "Zander Ion", "age": 20}]
+
+
+
